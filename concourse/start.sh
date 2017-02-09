634b299bd8f87f0b47f9f2843fb26db0b8144623
James Shingler <James.Shingler@CardinalHealth.com>
2017-02-09 17:09:29 -0500

    Working on test deploy

--- concourse/start.sh
+++ concourse/start.sh
@@ -4,4 +4,5 @@ ROOT_ADDRESS=${1:-`./whats_my_ip.sh`}
 echo "Provided external address is [${ROOT_ADDRESS}]"
 
 export CONCOURSE_EXTERNAL_URL=http://${ROOT_ADDRESS}:8080
-docker-compose up -d
+docker-compose up 
+# -d
