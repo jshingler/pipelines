b4c8f632dbc6ef6c4c63886c326d9118d79111b4
Jim Shingler <ShinglerJim@gmail.com>
2017-02-12 15:17:48 -0500

    pull artifact from embedded m2 repo

--- common/src/main/bash/test_deploy.sh
+++ common/src/main/bash/test_deploy.sh
@@ -18,7 +18,7 @@ projectGroupId=$( retrieveGroupId )
 projectArtifactId=$( retrieveArtifactId )
 
 # download app, eureka, stubrunner
-#JLS downloadJar 'true' ${REPO_WITH_JARS} ${projectGroupId} ${projectArtifactId} ${PIPELINE_VERSION}
+downloadJar 'true' ${REPO_WITH_JARS} ${projectGroupId} ${projectArtifactId} ${PIPELINE_VERSION}
 #JLS downloadJar ${REDEPLOY_INFRA} ${REPO_WITH_JARS} ${EUREKA_GROUP_ID} ${EUREKA_ARTIFACT_ID} ${EUREKA_VERSION}
 #JLS downloadJar ${REDEPLOY_INFRA} ${REPO_WITH_JARS} ${STUBRUNNER_GROUP_ID} ${STUBRUNNER_ARTIFACT_ID} ${STUBRUNNER_VERSION}
 # Log in to CF to start deployment
